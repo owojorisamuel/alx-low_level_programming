e00525e37690c0b2c840b6a6ec9bdacc55f2833c
owojori samuel <owososam4med@gmail.com>
2022-09-18 14:19:30 -0700

    new commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,18 @@
 
 /**
  *main - print lowercase a-z
- *Return: Always 0 (Success)
+ *Return: 0
  */
 
 int main(void)
 {
-	char alpha = 'a';
+	char alpha='a';
 
-	while (alpha <= 'z')
+	while (alpha<='z')
 		{
 			putchar(alpha);
 			alpha++;
 		}
 			putchar('\n');
-
 	return (0);
 }
